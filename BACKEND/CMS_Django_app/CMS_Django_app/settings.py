014b0433c58804871e03e32087325ab4722a13e7
raishwaryanair2011-2026 <raishwaryanair2011@gmail.com>
2026-02-27 15:34:04 +0530

    Edited Installed apps

--- BACKEND/CMS_Django_app/CMS_Django_app/settings.py
+++ BACKEND/CMS_Django_app/CMS_Django_app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Authentication',
+    'Admin',
+    'Receptionist',
+    'Pharmacist',
+    'Lab_Technician',
+    'Doctor'
 ]
 
 MIDDLEWARE = [
