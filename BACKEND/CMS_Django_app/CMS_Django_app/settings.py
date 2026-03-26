21252cfd9b56950c0ee07d467d6373c05110e561
Asif <mohamedasifiqbal2003@gmail.com>
2026-03-26 10:06:59 +0530

    db configure

--- BACKEND/CMS_Django_app/CMS_Django_app/settings.py
+++ BACKEND/CMS_Django_app/CMS_Django_app/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'CMS_Django_app.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "cms_project_demo_db",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
 
